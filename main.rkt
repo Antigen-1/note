779a47053c1cb08e76cc028de347fe6d6fe34059
Antigen-1 <official@antigen.top>
2022-09-04 11:28:46 +0800

    update

--- main.rkt
+++ main.rkt
@@ -1,18 +1,29 @@
 #lang racket/base
 (require (prefix-in o: string))
-(provide load-items compile-pattern match-to-filter)
+(provide (all-defined-out))
 
 (define load-items
-  (lambda ((directory (current-directory)) (filter-proc (lambda (item) #t)))
+  (lambda ((directory (current-directory)) (filter-proc values))
     (parameterize ((current-directory directory))
       (for/list ((file (in-list (filter file-exists? (directory-list)))))
         (cons file (filter filter-proc (dynamic-require (list 'file (path->string file)) 0)))))))
 
-(define compile-pattern
-  (lambda (pattern)
-    (o:compile-pattern pattern #:sequence->list string->list)))
-
 (define match-to-filter
-  (lambda (compiled #:equal? [equal? equal?])
+  (lambda (pattern #:equal? [equal? equal?])
+    (define compiled (o:compile-pattern pattern #:sequence->list string->list))
     (lambda (item)
       (o:Sunday-match item compiled #:sequence->list string->list #:equal? equal?))))
+
+(define compare-to-filter0
+  (lambda (pattern #:char=? [char=? char=?])
+    (define min-len (/ (length (regexp-match* #px"\\S" pattern)) 2))
+    (lambda (item)
+      (define res (o:get-longest-common-substring pattern item #:substring substring #:string->list string->list #:elem=? char=?))
+      (>= (length (regexp-match* #px"\\S" res)) min-len))))
+
+(define compare-to-filter1
+  (lambda (pattern #:char=? [char=? char=?])
+    (define min-len (/ (string-length pattern) 2))
+    (lambda (item)
+      (>= (o:longest-common-subsequence-length pattern item #:sequence->list string->list #:elem=? char=?)
+          min-len))))
