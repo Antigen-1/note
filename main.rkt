67118d524b362017c3ca29efc78a412183fb6f2d
Antigen-1 <official@antigen.top>
2022-09-12 16:55:38 +0800

    update

--- main.rkt
+++ main.rkt
@@ -13,17 +13,3 @@
     (define compiled (o:compile-pattern pattern #:sequence->list string->list))
     (lambda (item)
       (o:Sunday-match item compiled #:sequence->list string->list #:equal? equal?))))
-
-(define compare-to-filter0
-  (lambda (pattern #:char=? [char=? char=?])
-    (define min-len (/ (length (regexp-match* #px"\\S" pattern)) 2))
-    (lambda (item)
-      (define res (o:get-longest-common-substring pattern item #:substring substring #:string->list string->list #:elem=? char=?))
-      (>= (apply max (map (lambda (r) (length (regexp-match* #px"\\S" r))) res)) min-len))))
-
-(define compare-to-filter1
-  (lambda (pattern #:char=? [char=? char=?])
-    (define len (string-length pattern))
-    (lambda (item)
-      (= (o:longest-common-subsequence-length pattern item #:sequence->list string->list #:elem=? char=?)
-         len))))
