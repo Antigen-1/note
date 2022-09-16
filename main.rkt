5021a20b08691427e227b274b0b65c2a6997418e
Antigen-1 <official@antigen.top>
2022-09-16 23:15:35 +0800

    update

--- main.rkt
+++ main.rkt
@@ -1,15 +1,10 @@
 #lang racket/base
-(require (prefix-in o: string))
+(require string sugar/file racket/port racket/function)
 (provide (all-defined-out))
 
-(define load-items
-  (lambda ((directory (current-directory)) (filter-proc values))
+(define search
+  (lambda (pattern (directory (current-directory)))
+    (define compiled (compile-pattern pattern #:sequence->list string->list))
+    (define check (lambda (port) (Sunday-match port compiled #:sequence->list (curry port->list read-char))))
     (parameterize ((current-directory directory))
-      (for/list ((file (in-list (filter file-exists? (directory-list)))))
-        (cons file (filter filter-proc (dynamic-require (list 'file (path->string file)) 0)))))))
-
-(define match-to-filter
-  (lambda (pattern #:equal? [equal? equal?])
-    (define compiled (o:compile-pattern pattern #:sequence->list string->list))
-    (lambda (item)
-      (o:Sunday-match item compiled #:sequence->list string->list #:equal? equal?))))
+      (filter (lambda (path) (and (has-ext? path "nt") (call-with-input-file path (lambda (in) (check in))))) (directory-list)))))
--- parse.rkt
+++
@@ -1,9 +0,0 @@
-#lang racket/base
-(provide read read-syntax)
-
-(define read
-  (lambda ([in (current-input-port)])
-    (regexp-match* #px"(\\S+)\n\n" in #:match-select (compose bytes->string/utf-8 cadr))))
-(define read-syntax
-  (lambda ([src #f] [in (current-input-port)])
-    (datum->syntax #f (read in) #f)))
