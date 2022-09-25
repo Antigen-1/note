7af14df1dcecb844cb3b3e88975006ca8592f6d3
Antigen-1 <official@antigen.top>
2022-09-25 20:18:14 +0800

    update NCE

---
+++ 常用替换.html.pm
@@ -0,0 +1,22 @@
+#lang pollen/markup
+◊h1{常用替换}
+
+◊h2{叙述，讲述}
+
+◊lst{
+dl
+recount/relate/report sth to sb
+tell sb (about) sth
+narrate@记叙，强调按时间顺序——narrative记叙文
+account@其他搭配：account for占...比例；解释@形近词：discount打折，如get a 20% discount表示八折优惠
+}
+
+◊h2{传说}
+
+◊lst{
+ul
+saga长篇英雄传说
+epic史诗
+legend传奇——legendary传说中的
+myth[C]/mythology[U]神话
+}
