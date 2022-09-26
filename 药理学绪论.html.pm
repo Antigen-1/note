48c1b236f6b8e9fa1195d31fc506696debce430c
Antigen-1 <official@antigen.top>
2022-09-26 20:00:12 +0800

    add 药理学

---
+++ 药理学绪论.html.pm
@@ -0,0 +1,26 @@
+#lang pollen/markup
+◊h1{药理学绪论}
+
+◊h2{两大部分及其回答的问题}
+
+◊lst{
+dl
+药效学@治疗作用+不良反应=>indications（适应症）+mechanism+ADRs+contraindications（禁忌症）
+药动学@吸收+分布+代谢+排泄
+}
+
+◊h2{新药开发}
+
+◊tbl{
+概念辨析
+英文 中文
+in vitro@离体的，体外的
+in vivo@体内的
+in situ@原位的
+drug discovery，drug evaluation（R&G）@药物发现，药物评估
+clinical trial@临床试验
+}
+
+◊h2{如何学习}
+
+◊img{◊src{药理学学习方法.png}}
