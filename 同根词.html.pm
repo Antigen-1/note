9d8043a653a0a9b5d193de841d15c731d4f34062
Antigen-1 <official@antigen.top>
2022-10-07 20:04:37 +0800

    update NCE

---
+++ 同根词.html.pm
@@ -0,0 +1,63 @@
+#lang pollen/markup
+◊h1{同根词}
+
+◊h2{migration：n迁移，移居}
+
+◊lst{
+dl
+migrate@v
+migrant@n迁徙者
+immigrate to@v移民（进）
+immigration@n
+immigrant@n移民者
+emigrate from@v移民（出）
+emigration@n
+emigrant@n
+}
+
+◊(br)
+
+◊img[#:src "migra.png" #:alt "not supported" #:width "600" #:height "400"]
+
+◊(br)
+
+第一行与国境无关，另外两行均与国境有关。
+
+◊h2{anthropologist：n人类学家}
+
+同根名词anthropology
+
+◊(br)
+
+重要词根anthrop=man
+
+◊lst{
+dl
+anthropoid@类人的
+anthropogenic@人类导致/产生的
+philanthropist@慈善家@phil=love
+}
+
+重要词根（o）logy=to study
+
+◊lst{
+dl
+physiology@生理学
+archaeology@考古学@archaic：古雅的
+}
+
+◊h2{ancestor：n祖先}
+
+◊lst{
+dl
+an@ante表示“前”
+ces@to go
+tor@person
+}
+
+例如
+
+◊b{Yandi and Huangdi are the legendary ancestors of the Chinese nation.}
+
+炎帝和黄帝是中华民族的传奇的祖先。
+
--- 常用替换.html.pm
+++ 常用替换.html.pm
@@ -20,3 +20,31 @@ epic史诗
 legend传奇——legendary传说中的
 myth[C]/mythology[U]神话
 }
+
+◊h2{辈分}
+
+◊h3{祖先}
+
+◊lst{
+ul
+ancestor
+forefather
+forebear
+}
+
+◊h3{子孙}
+
+◊lst{
+dl
+descendant@后代
+offspring@子代（单复同型）
+posterity@U子孙
+}
+
+◊h2{腐烂}
+
+◊lst{
+dl
+rot@vt/vi@搭配：rot away烂掉@rotten（adj）eg.One rotten apple spoils the whole barrel.一颗老鼠屎毁了一锅粥。
+decay@同义词@派生义：衰败eg.Each civilization is born,it culminates,and it decays.
+}
