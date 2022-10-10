ff13d085fbe5c6a8cca9ccbee044be0ca0a20d83
Antigen-1 <official@antigen.top>
2022-10-10 20:09:36 +0800

    update index and NCE

---
+++ 文法.html.pm
@@ -0,0 +1,20 @@
+#lang pollen/markup
+◊h1{文法}
+
+◊h2{标题选词}
+
+◊h3{alliteration头韵}
+
+例如finding fossil man、beauty and the beast。
+
+◊h3{长度（音节）}
+
+◊tbl{
+替换
+短@长
+find@discover
+fossil man@ancient man、prehistoric man
+spur sb/sth （on）（to sth/to do sth）@encourage
+ban@prohibit、forbid
+vie （with sb）（for sth）@compete
+}
