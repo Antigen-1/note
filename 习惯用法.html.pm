f9304d50587a0e3ce456c1885a82149113a3d0b5
Antigen-1 <official@antigen.top>
2022-10-15 00:12:08 +0800

    update NCE

---
+++ 习惯用法.html.pm
@@ -0,0 +1,24 @@
+#lang pollen/markup
+◊h1{习惯用法}
+
+◊h2{避免头重脚轻}
+
+◊p{状语中一般地点在时间之前，但避免头重脚轻时可颠倒。}
+
+eg.[NCE4.1]◊b{...happened 5000 years in the Near East,where...}
+
+◊p{因为这个原因发生的颠倒需要仔细辨别。}
+
+eg1.◊b{What is an intellectual?I shall define him as an individual who has elected as his primary duty and pleasure in life the activity of thinking in Socratic way about moral problems.}
+
+◊(br)
+
+什么是知识分子？我会将其定义为这样一个个体：他选择将以苏格拉底的方式思考道德问题视为生活中首要的责任和乐趣。
+
+◊(br)
+
+eg2.◊b{As a logical consequence of this development,separate journals have now appeared aimed mainly towards either professional or amateur readership.}
+
+◊(br)
+
+作为这种发展的一种必然的结果，现在已经出现了主要针对专业或业余读者的分开的刊物。
