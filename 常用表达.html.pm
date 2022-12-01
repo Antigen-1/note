bfff9e33ad5ea52ccd9a828f4c2ccf8303d247fa
Antigen-1 <zhanghao@antigen.top>
2022-12-01 16:30:38 +0800

    update 英语

--- 常用表达.html.pm
+++ 常用表达.html.pm
@@ -367,3 +367,35 @@ pinnacle(建筑)尖顶，巅峰
 apex顶点，巅峰
 zenith穹顶，巅峰}
 
+◊h2{东}
+
+◊lst{
+ul
+the Near East：近东
+the Middle East：中东
+the Far East：远东
+}
+
+◊h2{传递}
+
+hand sth down (from ... to ...):pass sth from older people to younger ones
+
+◊h2{亲属的修饰}
+
+◊lst{
+ul
+remote ancestor
+distant relative(远亲)
+close/near relative(近亲)
+}
+
+◊h2{飞禽走兽相关表达}
+
+◊lst{
+ul
+skin皮
+hide兽皮
+fur毛皮
+feather羽毛
+}
+
