01bad7b68265a65f403628337ed272d480bf218a
Antigen-1 <zhanghao@antigen.top>
2022-12-03 17:29:16 +0800

    update 英语

--- 解题技巧.html.pm
+++ 解题技巧.html.pm
@@ -38,3 +38,34 @@ ul
 从句->并列句
 }
 
+◊h2{议论文}
+
+◊lst{ul
+切题「题目读三遍，否则不动笔」重点是◊lst{ul
+体裁
+话题
+内容要求}
+表达思想清楚，文字通顺连贯，即「逻辑」◊lst{ul
+首段引出话题、补充说明、过渡句
+中间经济、个人、社会
+尾段结论、建议、展望
+}
+无语言错误◊lst{ul
+词汇
+语法}}
+
+◊p{以上为基本要求}
+
+◊p{议论文又分为话题作文、名人名言、观点对比、图画作文。其中最基本的就是话题作文，其余只是在引出话题和补充说明方面有所不同。}
+
+◊p{如◊lst{dl
+话题作文@◊i{It is universially acknowledged that ... constitutes an indispensible part of our modern society and life.}@◊i{Recently,an increasing number of people come to join the heated discussion on ...}
+图画作文@“如图所示”◊i{As is vividly depicted/starkly portrayed in the cartoon,...（一般用现在进行时）}@◊i{Simple as the picture is,as a matterr of fact,this picture aims to reveal the fact that ...}
+名人名言“有句老话”@◊i{As an old saying goes,....Plain/Simple as the remark is,its profound meaning/implication turns out to be exceedingly far-reaching and thought-provoking.}
+观点对比@◊i{The issue/topic 同从 has drawn considerable attention nowadays.Some believe that ..., whereas others argue that .... From my perspective, I prefer the former/latter view.}}}
+
+◊p{通用内容◊lst{dl
+过渡句@◊i{A number of factors can account for this phenomenon/issue.}@◊i{My opinions go as follows.}
+建议句@◊i{It is imperative for ... , say, ... , to ... . Only in this way can we lead to the harmony of the society.
+中间举例@◊i{It's the experience of our remote ancestors, whereas it is still correct in many circumstances even today.}}}}
+
