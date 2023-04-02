f99d63a237a712ff12f5fb5893e9b5e5b41d57b3
Antigen-1 <zhanghao@antigen.top>
2023-04-02 16:44:58 +0800

    update NCE

--- 一词多义.html.pm
+++ 一词多义.html.pm
@@ -59,3 +59,23 @@ the French/Chinese authorities}
 ◊p{[n]公文包，文件夹；投资组合}
 
 ◊p{◊i{My stockbroker manages my portfolio for me.}我的股票经纪人为我管理投资组合}
+
+◊h2{convertible&conversion&convert}
+
+◊p{convertible [n]1.可兑换证券 2.敞篷汽车 [adj]可改变的}
+
+◊p{conversion [n]1. 改变，转变。the conversion of ... into ...:...到...的转变}
+
+◊p{◊i{the conversion of a barn into a house}畜棚变住宅}
+
+◊p{conversion [n]2. 改变信仰，皈依。one's conversion from ... to ...:由...到...的信仰转变}
+
+◊p{convert [v]1. 改变（形式或用途）[T/I] convert (sth) to/into sth}
+
+◊p{◊i{They converted the spare bedroom into an office}他们将空闲的卧室转变为了一间办公室}
+
+◊p{convert [v]2. 改变信仰[T/I] convert (sb) to sth}
+
+◊lst{ul
+◊i{European missionaries converted thousands to Christianity}欧洲传教士将成千上万人转变为了基督徒
+◊i{She converted to Catholicism}她变为了一个天主教徒}
