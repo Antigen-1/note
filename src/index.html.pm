0f2b580ac2a85ac6c1045f5b2f3fd7674efd59fa
ZhangHao <zhanghao@antigen.top>
2024-02-25 16:53:45 +0800

    Fix the index

--- src/index.html.pm
+++ src/index.html.pm
@@ -1,16 +1,15 @@
 #lang pollen/markup
 ◊h1{索引}
 
-◊(require racket/path)
-◊(define (make-html-list . items) `(ul ,@(map (lambda (i) `(li ,i)) (filter (lambda (i) (not (and (string? i) (string=? "" i)))) items))))
-◊(define (make-page-link rel) `(a ((href ,rel)) ,(path->string (file-name-from-path rel))))
+◊(require "pollen/pollen.rkt")
 
-◊make-html-list{
-◊make-page-link{pollen/影像学.html}
-◊make-page-link{pollen/英语.html}
-◊make-page-link{pollen/外科学.html}
-◊make-page-link{pollen/诊断学.html}
-◊make-page-link{pollen/内科学.html}
-◊make-page-link{pollen/传染病学.html}
-◊make-page-link{pollen/药理学.html}
+◊lst{
+ul
+◊lnk{pollen/影像学.html}
+◊lnk{pollen/英语.html}
+◊lnk{pollen/外科学.html}
+◊lnk{pollen/诊断学.html}
+◊lnk{pollen/内科学.html}
+◊lnk{pollen/传染病学.html}
+◊lnk{pollen/药理学.html}
 }
