1191b40b46a624843521e90efaaa4070ec657dba
ZhangHao <zhanghao@antigen.top>
2024-03-08 22:59:08 +0800

    Fix deps

--- info.rkt
+++ info.rkt
@@ -7,5 +7,5 @@
                    "web-server-lib"
                    "pollen"
                    "sugar"
-                   "raco/invoke"
+                   "raco-invoke"
                    "git://github.com/Antigen-1/hasket.git"))
